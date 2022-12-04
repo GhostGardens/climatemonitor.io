fdffa8e84eec85d9afa284211251da0c613aca75
Ben Guerard <ben.guerard@gmail.com>
2022-12-04 14:10:10 -0800

    Update goodmorning.py

--- goodmorning.py
+++ goodmorning.py
@@ -6,8 +6,8 @@ def get_ip_address():
     s.connect(("8.8.8.8", 80))
     return s.getsockname()[0]
 
-account_sid = "AC61781473c67d12f6c479d410f23d9238"
-auth_token = "d6509b7a46aff0fbcf68e8e820471ad8"
+account_sid = "{account SID}"
+auth_token = "{auth token}"
 my_name = socket.gethostname()
 my_ip = get_ip_address()
 
